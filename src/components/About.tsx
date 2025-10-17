58383ad0ece6a8693afe36c8aace3ab255680395
Kevin Morillo <kevinkemuelm448@gmail.com>
2025-10-17 15:18:51 -0400

    foto4

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -48,7 +48,7 @@ const About = () => {
                 <div className="relative">
                   <div className="w-64 h-64 md:w-72 md:h-72 rounded-2xl overflow-hidden glass-card p-1">
                     <img
-                      src="/assets/fotokevinportafolio.jpg"
+                      src="../assets/fotokevinportafolio.jpg"
                       alt="Kevin Morillo - Full Stack Developer"
                       className="w-full h-full object-cover rounded-xl"
                     />
